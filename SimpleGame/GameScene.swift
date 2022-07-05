ffe042e30705dc9b56d470edfbba75e9a8b6a704
Paolo ferrari <paoloferrariapple@gmail.com>
2022-07-05 12:26:31 +0200

    Base Version of a very simple SpriteKit game

--- SimpleGame/GameScene.swift
+++ SimpleGame/GameScene.swift
@@ -6,83 +6,117 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var player: SKSpriteNode!
+    var enemy: SKSpriteNode!
+    var leftMove: SKSpriteNode!
+    var leftPressing: Bool = false
+    var rightMove: SKSpriteNode!
+    var rightPressing: Bool = false
+    var jumpMove: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        //Visual Ground creation
+        let tempGround = SKSpriteNode(imageNamed: "Ground")
+        let groundRadius = tempGround.frame.width / 2.0
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        for i in stride(from: groundRadius, to: (scene?.frame.width)! + groundRadius, by: tempGround.frame.width) {
+            let ground = SKSpriteNode(imageNamed: "Ground")
+            ground.position = CGPoint(x: i, y: 50)
+            ground.zPosition = 0
+            addChild(ground)
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //Player creation
+        player = SKSpriteNode(imageNamed: "Character")
+        player.position = CGPoint(x: scene!.frame.width / 2, y: scene!.frame.height / 2)
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.restitution = 0.5
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        addChild(player)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame.inset(by: UIEdgeInsets(top: 50 + groundRadius, left: 0, bottom: 0, right: 0)))
+        
+        //move buttons
+        leftMove = SKSpriteNode (imageNamed: "Arrow")
+        leftMove.position = CGPoint(x:80, y:50)
+        leftMove.size = CGSize (width: 55, height: 55)
+        leftMove.name = "Left"
+        leftMove.zRotation = .pi / 2
+        leftMove.zPosition = 10
+        addChild(leftMove)
+        
+        rightMove = SKSpriteNode (imageNamed: "Arrow")
+        rightMove.position = CGPoint(x:160, y:50)
+        rightMove.size = CGSize (width: 55, height: 55)
+        rightMove.name = "Right"
+        rightMove.zRotation = -.pi / 2
+        rightMove.zPosition = 10
+        addChild(rightMove)
+        
+        
+        rightMove = SKSpriteNode (imageNamed: "Arrow")
+        rightMove.position = CGPoint(x: (scene?.frame.width)!-80, y:50)
+        rightMove.size = CGSize (width: 55, height: 55)
+        rightMove.name = "Jump"
+        rightMove.zPosition = 10
+        addChild(rightMove)
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch: AnyObject in touches {
+            let location = touch.location(in: self)
+            let node = self.atPoint(location)
+            
+            if (node.name == "Left") {
+                leftPressing = true
+                rightPressing = false
+            } else if (node.name == "Right") {
+                rightPressing = true
+                leftPressing = false
+            } else if (node.name == "Jump") {
+                for i in (5...17).reversed() {
+                    player.run(SKAction.sequence([SKAction.move(by: CGVector(dx: 0, dy: i), duration: 0.2)]))
+                }
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for touch: AnyObject in touches {
+            let location = touch.location(in: self)
+            let node = self.atPoint(location)
+            
+            if (node.name == "Left") {
+                leftPressing = false
+            } else if (node.name == "Right") {
+                rightPressing = false
+            }
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if leftPressing {
+            player.run(SKAction.sequence([SKAction.move(by: CGVector(dx: -3, dy: 0), duration: 0.1)]))
+        }
+        
+        if rightPressing {
+            player.run(SKAction.sequence([SKAction.move(by: CGVector(dx: 3, dy: 0), duration: 0.1)]))
+        }
+        
+        let rand = Int.random(in: 5...10)
+        if rand == 10 {
+            enemy = SKSpriteNode(imageNamed: "Enemy")
+            enemy.position = CGPoint(x: 25 + enemy.frame.width, y: scene!.frame.height - enemy.frame.height)
+            enemy.scale(to: CGSize(width: 35, height: 35))
+            enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+            enemy.physicsBody?.restitution = 0.8
+            enemy.physicsBody?.friction = 0
+            addChild(enemy)
+            enemy.run(SKAction.sequence([SKAction.wait(forDuration: 3), SKAction.removeFromParent()]))
+        }
     }
 }
